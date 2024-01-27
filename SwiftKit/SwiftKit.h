1defa3beecd3dad72a03a8cf7dfd731bacb2a71d
pengquanhuawork <132650889+pengquanhuawork@users.noreply.github.com>
2024-01-27 22:09:38 +0800

    Initial Commit

---
+++ SwiftKit/SwiftKit.h
@@ -0,0 +1,18 @@
+//
+//  SwiftKit.h
+//  SwiftKit
+//
+//  Created by quanhua peng on 2024/1/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftKit.
+FOUNDATION_EXPORT double SwiftKitVersionNumber;
+
+//! Project version string for SwiftKit.
+FOUNDATION_EXPORT const unsigned char SwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKit/PublicHeader.h>
+
+
